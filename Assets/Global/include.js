f349f9836f2bfb63987f534edd99e31525fa4dda
smymrr <yuuradevs@gmail.com>
2025-10-04 15:58:46 +0700

    Committed: Removed unncessary files and clickable logo

--- Assets/Global/include.js
+++
@@ -1,8 +0,0 @@
-function includeHTML(id, file) {
-  fetch(file)
-    .then(response => response.text())
-    .then(data => document.getElementById(id).innerHTML = data);
-}
-
-includeHTML("navbar", "Assets/Global/navbar.html");
-includeHTML("socials", "Assets/Global/socials.html");
